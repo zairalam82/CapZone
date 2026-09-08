a5f51c34d05e6f772d426c5ebb6e3aefe7419173
Zair Alam <zairalam82@gmail.com>
2026-09-08 10:22:11 +0500

    I have just write in the cart route this is my cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,3 +1,4 @@
+//This is my cart route 
 const express=require('express');
 const router=express.Router();
 const authenticateToken=require("../middleware/auth"); //ye abhi rehta hy bana hy 
@@ -7,4 +8,5 @@ const cartController=require("../mvc/cart mvc/controller/cartController");
 router.post('/',authenticateToken,cartController.addToCart); //agar token valid hy to controller ko req jaye gi
 
 module.exports=router;
+
 
