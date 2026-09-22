2f751d58209405d6834a061fa8a891265c7d8b1e
Zair Alam <zairalam82@gmail.com>
2026-09-23 01:50:13 +0500

    Only work on controller

--- mvc/order mvc/services/orderServices.js
+++ mvc/order mvc/services/orderServices.js
@@ -1,6 +1,8 @@
 const orderModel=require("../models/orderModel");
 
-exports.addToOrder=async(user_id,order_id,quantity)=>{
-    const orderData=await orderModel.addToOrder(user_id,order_id,quantity);
+exports.addToOrder=async(user_id)=>{
+    const orderData=await orderModel.addToOrder(user_id); //1
+    
+
     
 }
